32f783259c38d29432134dda23ceec4ce03facc1
Emanuel Ruidiaz <emafruidiaz@gmail.com>
2025-10-29 00:22:56 -0300

    project created

---
+++ TFIProgramacion2/src/main/java/com/mycompany/tfiprogramacion2/TFIProgramacion2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.tfiprogramacion2;
+
+/**
+ *
+ * @author ema_r
+ */
+public class TFIProgramacion2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
